acce3bc109f4ce8fa6b41a5a57fb52ddbcc18d20
LEE026 <lhs456852@gmail.com>
2022-05-23 16:53:06 +0900

    feat:Added ability to move array and zoom in and out

--- animationHelper.js
+++ animationHelper.js
@@ -1,4 +1,4 @@
-const TERM=5;
+const DRAW_TERM=5;
 class animationHelper {
   constructor(drawObj){
     this.drawObj=drawObj;
@@ -6,12 +6,12 @@ class animationHelper {
 
   move(obj,toX,toY,time){
     let nowPos=obj.getPos();
-    let step=parseInt(time/TERM);
+    let step=parseInt(time/DRAW_TERM);
     let speedX=(toX-nowPos[0])/step;
     let speedY=(toY-nowPos[1])/step;
 
     for(let i=1;i<step;i++){
-      setTimeout(()=>{obj.setPos(nowPos[0]+speedX*i,nowPos[1]+speedY*i);this.drawObj.draw()},TERM*i);
+      setTimeout(()=>{obj.setPos(nowPos[0]+speedX*i,nowPos[1]+speedY*i);this.drawObj.draw()},DRAW_TERM*i);
     }
     setTimeout(()=>{obj.setPos(toX,toY);this.drawObj.draw()},time);
 
