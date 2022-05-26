bda37ab19d72a3face2bbb3db1124a7a28f2116c
LEE026 <lhs456852@gmail.com>
2022-05-26 21:47:28 +0900

    feat:insertionSort

---
+++ InsertionSort/tracking.js
@@ -0,0 +1,19 @@
+const CID=13;
+const NAME="삽입정렬"
+
+function onGeoOk(position){
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  makePixel(lat,lng);
+}
+function onGeoError(){
+  makePixel();
+}
+
+function makePixel(lat=null,lng=null){
+  let img='http://www.google-analytics.com/collect?v=1&tid=UA-186810924-1&t=event&cid='+CID+'&&cs=page&ec='+NAME+'&el='+lat+","+lng;
+  console.log(img);
+  document.getElementById("tracking").src=img;
+
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError );
