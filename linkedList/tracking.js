581fcdd7fd497890e59e6c6c4f6e79ca61ecf41b
DDIONG <12yangyang25@naver.com>
2022-06-04 22:24:14 +0900

    feat: add tacking.js

---
+++ linkedList/tracking.js
@@ -0,0 +1,28 @@
+const offset = 4000;
+let cidRandomInt = parseInt(Math.random() * 1000 + offset);
+const CID = cidRandomInt;
+const NAME = "연결리스트";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  makePixel(lat, lng);
+}
+function onGeoError() {
+  makePixel();
+}
+
+function makePixel(lat = null, lng = null) {
+  let img =
+    "http://www.google-analytics.com/collect?v=1&tid=UA-186810924-1&t=event&cid=" +
+    CID +
+    "&&cs=page&ec=" +
+    NAME +
+    "&el=" +
+    lat +
+    "," +
+    lng;
+  console.log(img);
+  document.getElementById("tracking").src = img;
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
