02838705d63ca71e3f79f89e6df5e49b3de6393f
alirz-pixel <alirce@naver.com>
2022-06-15 04:34:56 +0900

    add: introduce text on about page (#7)

--- src/js/About.js
+++ src/js/About.js
@@ -21,3 +21,18 @@ document.addEventListener('keyup', (e) => {
 	screen_cnt = Math.max(MIN_SCREEN, screen_cnt);
 	screen_cnt = Math.min(MAX_SCREEN, screen_cnt);
 })
+
+let is_introduce_text_block = false;
+$(document).ready(() => {
+	$('.introduce-warp img').click((e) => {
+		if (is_introduce_text_block) {
+			$('.introduce-text').css('opacity', '0');
+		}
+		else {
+			$('.introduce-text').html(e.target.getAttribute('data-intro-text'));
+			$('.introduce-text').css('opacity', '1');
+		}
+
+		is_introduce_text_block = !is_introduce_text_block;
+	}) 
+});
